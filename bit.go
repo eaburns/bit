1e214bbd3ea508f150349222addaff3143db16b1
mewmew <rnd0x00@gmail.com>
2013-10-29 15:12:52 +0100

    Correct some spelling in comments.

--- bit.go
+++ bit.go
@@ -16,12 +16,12 @@ type Reader struct {
 	n   uint
 }
 
-// NewReader returns a new Reader that reads bits the given io.Reader.
+// NewReader returns a new Reader that reads bits from the given io.Reader.
 func NewReader(r io.Reader) *Reader {
 	return &Reader{in: r}
 }
 
-// Read returns the next n bits, up to 64.  It panicks if n is greater than 64.
+// Read returns the next n bits, up to 64.  It panics if n is greater than 64.
 // If an EOF is encountered and some bits have been read then io.ErrUnexpectedEOF
 // is returned, otherwise if an EOF is encountered and nothing was read then io.EOF
 // is returned.
@@ -90,9 +90,9 @@ func mask(i uint) uint64 {
 // ReadFields performs a series of reads of differing numbers of bits, specified by the argument
 // list.  If no error occurs, then the results of the reads are returned in a slice, otherwise the
 // first encountered error is returned.   If the error is EOF and any number of bits has been
-// beer read then io.ErrUnexpectedEOF is returned, if the error is EOF and nothing was read
+// read then io.ErrUnexpectedEOF is returned, if the error is EOF and nothing was read
 // then io.EOF is returned.  If any of the read sizes specified by the argument are greater than
-// 64 then ReadFields panicks.
+// 64 then ReadFields panics.
 func (r *Reader) ReadFields(ns ...uint) ([]uint64, error) {
 	fs := make([]uint64, len(ns))
 	var err error
